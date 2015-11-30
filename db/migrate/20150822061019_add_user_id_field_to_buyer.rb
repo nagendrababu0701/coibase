c31fd9f43443858f00634b3cd8896deb19ca86e5
dheerajCH <dkumar@spinovation.com>
2015-12-01 01:39:26 +0530

    testing bitcoin app

--- db/migrate/20150822061019_add_user_id_field_to_buyer.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdFieldToBuyer < ActiveRecord::Migration
-  def change
-    add_column :buyers, :user_id, :integer
-    add_column :buyers, :bitcoin_address, :boolean
-  end
-end
--- db/migrate/20150822061313_add_captha_filed_to_seller.rb
+++
@@ -1,6 +0,0 @@
-class AddCapthaFiledToSeller < ActiveRecord::Migration
-  def change
-    add_column :sellers, :captcha, :string
-    add_column :sellers, :captcha_key, :string
-  end
-end
