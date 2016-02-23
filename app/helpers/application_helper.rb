079abcf7b49c36c9bbdf461a9bcdd07dd7d10c13
amitkumarpatel <amitkumarpatel44@gmail.com>
2016-02-24 00:00:10 +0530

    updated design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+# def add_active_class_to_header_menu_item
+#  class="active"
+# end
+
+ def active_class(link_path)
+  current_page?(link_path) ? "active" : ""
+ end
+ 
 end
